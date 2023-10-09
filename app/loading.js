e6bca8cb7f65589435f0cc3bfe44b41152c2d9d3
Kevin Abraham <138606225+metadol@users.noreply.github.com>
2023-10-09 08:46:49 +0530

    Updated loading.js

--- app/loading.js
+++ app/loading.js
@@ -1,5 +1,5 @@
 export default function Loading() {
-    // You can add any UI inside Loading, including a Skeleton.
+    
     return (
      <div className='flex h-screen justify-center items-center'>
         <div className="rounded-md h-12 w-12 border-4 border-t-4 border-blue-500 animate-spin absolute">
